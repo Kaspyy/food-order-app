f6b14f17d70efca7299f96ae0dcc3d83ef9201de
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2022-04-23 14:23:30 +0200

    firebase rest api to get and post data

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -56,6 +56,11 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
+  }
+
+  return defaultCartState;
 };
 
 const CartProvider = props => {
@@ -77,11 +82,18 @@ const CartProvider = props => {
     });
   };
 
+  const clearCartHandler = () => {
+    disptachCartAction({
+      type: 'CLEAR',
+    });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
